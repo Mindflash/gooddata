b4659583bda0460d4651732db7306c86931166c6
Jon Sullivan <jon@mindflash.com>
2013-12-30 09:04:51 -0800

    Added example config (replace with your own values)

---
+++ tests/config/config-example.js
@@ -0,0 +1,7 @@
+module.exports = {
+	projectId: 'asdfa3asdf098as09df80a9sd8f09as8',
+	username: 'gooddata@mydomain.com',
+	password: 'asdfa0s9df09a8s0df98a0s9',
+	userId: '/gdc/account/profile/lasdfoiuasd98f09as8df098s0d9s9df',
+	filter: '/gdc/md/s0d9f8s09df8s09df8s09df80s9d8fd9/obj/983472'
+}
