b297ce0b71529c41dcf3a759be02ce09b60a0a97
sathiz <sathizz@gmail.com>
2018-04-23 17:30:52 -0700

    chore: update version with ES6

--- tests/authTests.js
+++ tests/authTests.js
@@ -4,40 +4,40 @@ var util = require('util');
 var gooddata = require('../lib/api.js');
 
 // TO RUN TESTS REPLACE THE CONFIG WITH YOUR GOODDATA CREDENTIALS
-var config = require('./config/config.js');
+var config = require('./config/config');
 
 function getInvalidAPI() {
-	return gooddata.createClient({
-		projectId: 'TEST PROJ',
-		username: 'TEST USER',
-		password: 'TEST PWD'
-	});
+  return gooddata.createClient({
+    projectId: 'TEST PROJ',
+    username: 'TEST USER',
+    password: 'TEST PWD'
+  });
 }
 
 function getAPI() {
-	return gooddata.createClient(config);
+  return gooddata.createClient(config);
 }
 
-test('invalid login attempt to gooddata returns err', function(t) {
-	var api = getInvalidAPI();
-	var gdOptions = {};
+test('invalid login attempt to gooddata returns err', function (t) {
+  var api = getInvalidAPI();
+  var gdOptions = {};
 
-	api.updateFilters(gdOptions, function (err, res) {
-		t.ok(err, 'error on invalid login attempt ' + util.inspect(err));
-		t.notOk(res, 'should not have responded ' + util.inspect(res));
-		t.end();
-	});
+  api.updateFilters(gdOptions, function (err, res) {
+    t.ok(err, 'error on invalid login attempt ' + util.inspect(err));
+    t.notOk(res, 'should not have responded ' + util.inspect(res));
+    t.end();
+  });
 });
 
-test('valid login to gooddata', function(t) {
-	var api = getAPI();
-	var gdOptions = {
-		userId: config.userId,
-		filters: [ config.filter ]
-	};
+test('valid login to gooddata', function (t) {
+  var api = getAPI();
+  var gdOptions = {
+    userId: config.userId,
+    filters: [config.filter]
+  };
 
-	api.updateFilters(gdOptions, function (err, res) {
-		t.notOk(err, 'no errors logging in ' + util.inspect(err));
-		t.end();
-	});
+  api.updateFilters(gdOptions, function (err, res) {
+    t.notOk(err, 'no errors logging in ' + util.inspect(err));
+    t.end();
+  });
 });
--- tests/config/config-example.js
+++ tests/config/config-example.js
@@ -1,9 +1,9 @@
 module.exports = {
-	projectId: 'asdfa3asdf098as09df80a9sd8f09as8',
-	username: 'gooddata@mydomain.com',
-	password: 'asdfa0s9df09a8s0df98a0s9',
-	userId: '/gdc/account/profile/lasdfoiuasd98f09as8df098s0d9s9df',
-	filter: '/gdc/md/asdfa3asdf098as09df80a9sd8f09as8/obj/983472',
-	attributeUri: '/gdc/md/asdfa3asdf098as09df80a9sd8f09as8/obj/1219',
-	titles: ['title1','title2']
-}
+  projectId: 'asdfa3asdf098as09df80a9sd8f09as8',
+  username: 'gooddata@mydomain.com',
+  password: 'asdfa0s9df09a8s0df98a0s9',
+  userId: '/gdc/account/profile/lasdfoiuasd98f09as8df098s0d9s9df',
+  filter: '/gdc/md/asdfa3asdf098as09df80a9sd8f09as8/obj/983472',
+  attributeUri: '/gdc/md/asdfa3asdf098as09df80a9sd8f09as8/obj/1219',
+  titles: ['title1', 'title2']
+};
