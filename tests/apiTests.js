784c4a514d0a75f9ff5c93347f4389c0602468bc
Sudin Maharjan <sathizz@gmail.com>
2018-05-02 17:58:24 -0400

    chore: update version with ES6 (#5)

--- tests/apiTests.js
+++ tests/apiTests.js
@@ -9,59 +9,59 @@ var config = require('./config/config.js');
 
 var api = gooddata.createClient(config);
 
-test('updating filters', function(t) {
-	var params = {
-		userId: config.userId,
-		filters: [ config.filter ]
-	};
+test('updating filters', function (t) {
+  var params = {
+    userId: config.userId,
+    filters: [config.filter]
+  };
 
-	api.updateFilters(params, function (err, res) {
-		t.notOk(err, 'no errors logging in ' + util.inspect(err));
-		t.ok(res, 'updated filters successfully ' + util.inspect(res));
-		t.end();
-	});
+  api.updateFilters(params, function (err, res) {
+    t.notOk(err, 'no errors logging in ' + util.inspect(err));
+    t.ok(res, 'updated filters successfully ' + util.inspect(res));
+    t.end();
+  });
 });
 
-test('get attribute by uri', function(t) {
-	var params = {uri: config.attributeUri};
+test('get attribute by uri', function (t) {
+  var params = { uri: config.attributeUri };
 
-	api.getAttributeByUri(params, function (err, res) {
-		t.notOk(err, 'no errors logging in ' + util.inspect(err));
-		t.ok(res, 'got response ' + util.inspect(res));
-		t.end();
-	});
+  api.getAttributeByUri(params, function (err, res) {
+    t.notOk(err, 'no errors logging in ' + util.inspect(err));
+    t.ok(res, 'got response ' + util.inspect(res));
+    t.end();
+  });
 });
 
-test('get attribute\'s elements by title when passing attribute uri', function(t) {
-	var params = {
-		titles: config.titles,
-		uri: config.attributeUri
-	};
+test('get attribute\'s elements by title when passing attribute uri', function (t) {
+  var params = {
+    titles: config.titles,
+    uri: config.attributeUri
+  };
 
-	api.findAttributeElementsByTitle(params, function (err, res) {
-		t.notOk(err, 'no errors logging in ' + util.inspect(err));
-		t.ok(res, 'got response ' + util.inspect(res));
-		t.ok(_.keys(res).length > 0, 'found elements');
-		t.end();
-	});
+  api.findAttributeElementsByTitle(params, function (err, res) {
+    t.notOk(err, 'no errors logging in ' + util.inspect(err));
+    t.ok(res, 'got response ' + util.inspect(res));
+    t.ok(_.keys(res).length > 0, 'found elements');
+    t.end();
+  });
 });
 
-test('get attribute\'s elements by title when passing attribute object', function(t) {
-	var getAttributeParams = {uri: config.attributeUri};
-	var findAttributeElementsParams = {
-		titles: config.titles,
-		uri: config.attributeUri
-	};
+test('get attribute\'s elements by title when passing attribute object', function (t) {
+  var getAttributeParams = { uri: config.attributeUri };
+  var findAttributeElementsParams = {
+    titles: config.titles,
+    uri: config.attributeUri
+  };
 
-	api.getAttributeByUri(getAttributeParams, function (err, res) {
-		t.notOk(err, 'no errors logging in ' + util.inspect(err));
-		t.ok(res, 'got response ' + util.inspect(res));
+  api.getAttributeByUri(getAttributeParams, function (err, res) {
+    t.notOk(err, 'no errors logging in ' + util.inspect(err));
+    t.ok(res, 'got response ' + util.inspect(res));
 
-		api.findAttributeElementsByTitle(findAttributeElementsParams, function (err, res) {
-			t.notOk(err, 'no errors logging in ' + util.inspect(err));
-			t.ok(res, 'got response ' + util.inspect(res));
-			t.ok(_.keys(res).length > 0, 'found elements');
-			t.end();
-		});
-	});
+    api.findAttributeElementsByTitle(findAttributeElementsParams, function (err, res) {
+      t.notOk(err, 'no errors logging in ' + util.inspect(err));
+      t.ok(res, 'got response ' + util.inspect(res));
+      t.ok(_.keys(res).length > 0, 'found elements');
+      t.end();
+    });
+  });
 });
